bf6ca9995aca68c2af95880e59a744cffc19dfd8
Caio Oliveira <caiomcoliveira@gmail.com>
2017-11-13 23:05:20 -0200

    estatisticas

--- stats.py
+++ stats.py
@@ -6,23 +6,49 @@
 
 from scipy import stats
 import sqlite3
+import sys
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 class VideosData:
-    views = [] #lista vazia
-    isQuestion = []  #lista vazia
-    isArrowCircle = []
-    isSexual = []
-    isHyperbole = []
-    containsBaitWords = []
-    dates = []
+    def __init__(self):
+        self.views = []  # lista vazia
+        self.isQuestion = []  # lista vazia
+        self.isArrowCircle = []
+        self.isSexual = []
+        self.isHyperbole = []
+        self.containsBaitWords = []
+        self.dates = []
+        self.rating = []
+        self.ratings = []
+        self.isClickBait = []
+
+    def fillClickBait(self):
+        for i in range(len(self.isQuestion)):
+            self.isClickBait.append(
+                self.isQuestion[i] or self.isArrowCircle[i] or self.isSexual[i] or self.isHyperbole[i] or self.containsBaitWords[i])
 
 
-
-def getDataByUser(user):    
+def getDataByUser(user):
     conn = sqlite3.connect('socialblade.db')
     c = conn.cursor()
-    media = c.execute("SELECT avg(views) FROM videos where userId = '{}'and created_at <= '2017-11-15';".format(user)).fetchone()       
-    results = c.execute("SELECT views, isQuestion, isArrowCircle, isSexual, isHyperBole, containsBaitWords, created_at FROM videos where userId = '{}' and created_at <= '2017-11-15';".format(user)).fetchall()   
-    data = VideosData()        
+    media = c.execute(
+        "SELECT avg(views) FROM videos where userId = '{}' order by created_at asc LIMIT 40;".format(user)).fetchone()
+
+    results = c.execute(
+        "SELECT views, isQuestion, isArrowCircle, isSexual, isHyperbole, containsBaitWords, rating, ratings, created_at FROM videos where userId = '{}' order by created_at asc LIMIT 40;".format(user)).fetchall()
+    data = VideosData()
+
     for result in results:
         data.views.append(media[0] - result[0])
         data.isQuestion.append(result[1])
@@ -30,16 +56,34 @@ def getDataByUser(user):
         data.isSexual.append(result[3])
         data.isHyperbole.append(result[4])
         data.containsBaitWords.append(result[5])
-        data.dates.append(result[6])    
-    return data
+        data.rating.append(result[6])
+        data.ratings.append(result[7])
+        data.dates.append(result[8])
+    data.fillClickBait()
     conn.close()
+    return data
 
 
+users = ['jakepaulproductions', 'pewdiepie',
+         'rezendeevil', 'jacksepticeye', 'felipeneto']
 
-user = 'jakepaulproductions'
-data = getDataByUser(user)
-print("Correlation Question = {}").format(stats.pointbiserialr(data.views,data.isQuestion))
-print("Correlation ArrowCircle = {}").format(stats.pointbiserialr(data.views,data.isArrowCircle))
-print("Correlation Sexual = {}").format(stats.pointbiserialr(data.views,data.isSexual))
-print("Correlation Hyperbole = {}").format(stats.pointbiserialr(data.views,data.isHyperbole))
-print("Correlation containsBaitWords = {}").format(stats.pointbiserialr(data.views,data.containsBaitWords))
+for user in users:
+    data = getDataByUser(user)
+    print(bcolors.OKGREEN + "##############" +
+          user + "#############" + bcolors.ENDC)
+    print("Correlation Question = " +
+          repr(stats.pointbiserialr(data.views, data.isQuestion)))
+    print("Correlation ArrowCircle = " +
+          repr(stats.pointbiserialr(data.views, data.isArrowCircle)))
+    print("Correlation Sexual = " +
+          repr(stats.pointbiserialr(data.views, data.isSexual)))
+    print("Correlation Hyperbole = " +
+          repr(stats.pointbiserialr(data.views, data.isHyperbole)))
+    print("Correlation containsBaitWords = " +
+          repr(stats.pointbiserialr(data.views, data.containsBaitWords)))
+    print("Correlation bait = " +
+          repr(stats.pointbiserialr(data.views, data.isClickBait)))
+    print("Coef Peason with ratings" +
+          repr(stats.pearsonr(data.views, data.ratings)))
+    print("Coef Peason with rating (%)" +
+          repr(stats.pearsonr(data.views, data.rating)))
